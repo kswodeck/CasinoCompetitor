3edf32449fd0af0dae7a94814c0ae4adce4562f7
Kris <kswodeck@yahoo.com>
2020-02-02 23:45:52 -0800

    update

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,6 +1,7 @@
 var handsDealt = 0;
 var cards = [];
 var cards2 = [];
+var currentHand = cards;
 var rankAchieved = false;
 
 function getCards() {
@@ -18,8 +19,10 @@ function getCards() {
     return randomValues;
   }
   for (let currentCard=0; currentCard<5; currentCard++){
+    // let first = 10;
     if (handsDealt===0)
     {
+      currentHand = cards;
       document.getElementById("hold"+currentCard).style.opacity = 0.0001;
       let isSameIdentity=true;
       while (isSameIdentity===true)
@@ -29,8 +32,15 @@ function getCards() {
             isSameIdentity=false;
           }
           currentValues = getRandomCardValues();
+          // if (currentCard==0){
+          //   currentValues = [1,3];
+          // }
+          // else {
+          //   currentValues = [first,3];
+          // }
           numValue = currentValues[0]; //13 card value options excluding jokers. 1=ace, 11=jack, 12=queen, 13=king
           numSuit = currentValues[1]; //4 card suit options excluding jokers. club,diamond,heart,spade
+          // first++;
           identity = numValue+"-"+numSuit;
           if (cards.filter(x => (x.identity === identity)).length === 0){
             isSameIdentity=false;
@@ -42,6 +52,7 @@ function getCards() {
     }
     else if (handsDealt==1)
     {
+      currentHand = cards2;
       document.getElementById("hold"+currentCard).style.opacity = 0.0001;
       let isSameIdentity=true;
       while (isSameIdentity===true)
@@ -72,14 +83,6 @@ function getCards() {
         if (currentCard == 4) {
           cards = [];
           cards2 = [];
-          if (rankAchieved === true)
-          {
-            // document.getElementById("hand-ranking-heading").style.color = "darkblue"; if a hand category has been acheived, blue text
-          }
-          else
-          {
-            // document.getElementById("hand-ranking-heading").style.color = "crimson"; if no hand categories have been acheived, red text
-          }
           document.getElementById("hand-ranking-heading").style.display = "block";
         }
     }
@@ -90,7 +93,18 @@ function getCards() {
       document.getElementById("card-deal-button").innerText = "Deal Cards";
     }
   }
-
+  if (handsDealt==0 || handsDealt==1)
+  {
+    let rankingText = getHandRanking(currentHand);
+    if (rankAchieved === true)
+    {
+      document.getElementById("hand-ranking-heading").style.color = "darkblue"; // if a hand category has been acheived, blue text
+    }
+    else
+    {
+      document.getElementById("hand-ranking-heading").style.color = "crimson"; // if no hand categories have been acheived, red text
+    }
+  }
   handsDealt++;
 }
 
@@ -159,33 +173,76 @@ function isEqual(v1, v2)
   {return true} else {return false}
 }
 
-function getHandRanking(currentHand){
-  let isRoyalHand = true;
-  let isStraightHand = true;
-  let isFlushHand = true;
+function getHandRanking(hand){
+  let handRanking = "Game Over";
+  let isFlush = isFlushHand(hand);
+  let isRoyal = isRoyalHand(hand);
+  let isStraight = isStraightHand(hand[0].numValue,hand[1].numValue,hand[2].numValue,hand[3].numValue,hand[4].numValue);
   let isJackOrBetterHand = true;
   let isFullHouse = false;
-  for (let currentCard=0; currentCard<5; currentCard++){
+  for (let currentCard=0; currentCard<=4; currentCard++){
     for (let comparedCard=4; comparedCard>=0; comparedCard--)
     {
       if (currentCard != comparedCard){
-        if (currentHand[currentCard].numSuit == currentHand[comparedCard].numSuit){ //conditional to determine flush (royal, straight, normal)
+        if (isFlush===true){ //conditional to determine flush (royal, straight, normal)
+          rankAchieved = true;
           isJackOrBetterHand = false;
-          if (currentHand[currentCard].isRoyalHand == true) //determines if royal flush
+          if (isRoyal == true) //determines if royal flush
           {
-            alert("Royal Flush");
+            alert("Royal Flush"); //working
+            return handRanking = "Royal Flush";
           }
-          else if (isStraightHand == true){ //determines if straight flush, not royal. Difficult to determine. Need each card to be 1 apart
-            isRoyalHand = false;
-            alert("Straight Flush");
+          else if (isStraight == true){ //determines if straight flush, not royal. Difficult to determine. Need each card to be 1 apart
+            alert("Straight Flush"); //working
+            return handRanking = "Straight Flush";
           }
           else { //hand is just a normal flush
-            isStraightHand = false;
-            isRoyalHand = false;
-            alert("Flush");
+            alert("Flush"); //working
+            return handRanking = "Flush";
           }
         }
       }
+    }
+  }
+}
+
+function isFlushHand(hand){
+  // let sameSuitCount = 0;
+  // let isFlush = currentHand.every(isEqual(currentHand[0].numSuit))
+  if (isEqual(hand[0].numSuit,hand[1].numSuit) && isEqual(hand[1].numSuit,hand[2].numSuit)
+  && isEqual(hand[2].numSuit,hand[3].numSuit) && isEqual(hand[3].numSuit,hand[4].numSuit))
+  {
+    return true;
+  }
+  else {
+    {
+      return false;
+    }
+  }
+}
+
+function isStraightHand(v0,v1,v2,v3,v4){
+  let compArray = [v0,v1,v2,v3,v4];
+  let minNum = Math.min(v0,v1,v2,v3,v4);
+    if (compArray.includes(minNum+1) && compArray.includes(minNum+2) && compArray.includes(minNum+3) && compArray.includes(minNum+4))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+function isRoyalHand(hand){
+  if (hand[0].isRoyal==true && hand[1].isRoyal==true &&
+     hand[2].isRoyal==true && hand[3].isRoyal==true && hand[4].isRoyal==true)
+  {
+    return true;
+  }
+  else {
+    {
+      return false;
     }
   }
 }
