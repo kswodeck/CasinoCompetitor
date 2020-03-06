46897c8ac2bd929b0a53e1387d9635c06b11b31a
Kris <kswodeck@yahoo.com>
2020-03-05 22:02:12 -0800

    update

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,11 +1,9 @@
-const inputs = document.getElementsByClassName('create-account-input');
-const validitySpans = document.getElementsByClassName('valid-content');
-const invalidList = document.getElementById('invalid-fields-list');
-const newEmail = document.getElementById('newEmail');
-const newPassword = document.getElementById('newPassword');
-const repeatPassword = document.getElementById('repeatPassword');
-
 function validateAcccountCreate() {
+    const inputs = document.getElementsByClassName('create-account-input');
+    const validitySpans = document.getElementsByClassName('valid-content');
+    const invalidList = document.getElementById('invalid-fields-list');
+    const newPassword = document.getElementById('newPassword');
+    const repeatPassword = document.getElementById('repeatPassword');
     function addToInvalidList(str, el) {
         const item = document.createElement('li');
         item.className = 'invalid-list';
@@ -33,3 +31,23 @@ function validateAcccountCreate() {
         validitySpans[3].style.color = 'crimson';
     }
 }
+
+function validateKeys(evt, type) {
+    var theEvent = evt || window.event;
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+    // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\./;
+    if (type == 'tel') {
+        regex = /[0-9]|[-]|[+]|[(]|[)]|[ ]/;
+    }
+    if(!regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+  }
