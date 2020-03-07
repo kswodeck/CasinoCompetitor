adc2e8024a1b08ecaddad02493e4777672cdd69e
Kris <kswodeck@yahoo.com>
2020-03-07 13:47:57 -0800

    up

--- app.js
+++ app.js
@@ -41,13 +41,13 @@ var posts = [
 
 // error handler
 // app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  // res.status(err.status || 500);
-  // res.render('error');
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
 // });
 
 app.use(express.static('public'));
