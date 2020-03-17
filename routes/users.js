ae32067037b5f712f60c95b3452c71fc4562bde4
kswodeck <kswodeck@yahoo.com>
2020-03-17 15:16:52 -0700

    up

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('../public/structure/home.html');
+  res.send('../public/home.html');
 });
 
 module.exports = router;
