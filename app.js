18839a125b1be72aa508d8f67a06482f847329cc
Kristoffer Swodeck <kswodeck@mindresearch.org>
2020-04-08 12:25:37 -0700

    up

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ var express      = require('express'),
   User           = require('./models/user'),
   userRoutes     = require('./routes/users'),
   featureRoutes  = require('./routes/features');
-  
-// var createError = require('http-errors');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
 
 const connectionString = 'mongodb+srv://kswodeck:Kmswo123!@pocketpoker1-zl3ub.mongodb.net/pocketpoker?retryWrites=true&w=majority';
 const hostname = '127.0.0.1';
@@ -56,57 +51,6 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   return res.status(500).render('error', {pageTitle: 'Server Error', message: err, status: 'Server Error: 500'});
 });
-
-// async function createUser(username) {
-//   return new User({
-//     username,
-//     created: Date.now()
-//   }).save()
-// }
-
-// async function findUser(username) {
-//   return await User.findOne({ username })
-// }
-
-// ;(async () => {
-//   const connector = mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
-//   const username = process.argv[2].split('=')[1];
-
-//   let user = await connector.then(async () => {
-//     return findUser(username);
-//   })
-
-//   if (!user) {
-//     user = await createUser(username);
-//   }
-
-//   console.log(user);
-//   process.exit(0);
-// })();
-
-// app.set('views', path.join(__dirname, 'views'));
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({extended: false}));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 var server = app.listen(port, hostname, function(){
   console.log('App running on ' + hostname + ':' + port)
