6a712503a9db5317e9c39a4c47bb94d40eb9d33e
Kris <kswodeck@yahoo.com>
2020-05-03 23:23:19 -0700

    up

--- public/javascripts/farkle.js
+++ public/javascripts/farkle.js
@@ -3,63 +3,77 @@ var diceArr = [];
 var holdArr = [];
 var rollRemaining = true;
 var diceRolls = 0;
+var diceHeld = 0;
 var currentRollScore = 0;
 var currentBet = 50;
 var diceRollDiv = document.getElementById('farkle-roll-div');
 var diceRollButton = document.getElementById('farkle-roll-button');
 var farkleEndButton = document.getElementById('farkle-end-button');
-var handRankingHeading = document.getElementById('roll-ranking-heading');
+var rollRankingHeading = document.getElementById('roll-ranking-heading');
 var rollScoreHeading = document.getElementById('roll-score-heading');
+var rollScoreDiv = document.getElementById('farkle-roll-score-div');
+
 
 function farkleRoll() {
   diceRollButton.disabled = true;
-  if (handRankingHeading.innerText == 'Farkle') {
-    rollRemaining = false;
-    diceRollButton.innerText = 'Play Again';
-    farkleEndButton.setAttribute('disabled');
-  }
-  if (diceRolls > 0 && rollRemaining == true) {
+  if (diceRolls > 0) {
+    var heldDice = 0;
     for (let curDice = 0; curDice < 6; curDice++) {
       if (diceArr[curDice].isHeld) {
-        console.log(diceArr[curDice]);
-        console.log('is held');
         holdArr.push(diceArr[curDice]);
+        document.getElementById('hold' + curDice).classList.add('text-opacity');
+        heldDice++;
       }
     }
+    if (heldDice == diceHeld) { //this means no additional dice were held this round
+      diceRolls == 1;
+      diceRollButton.disabled = false;
+      return; //display a popup here. Exit the function
+    }
+    diceHeld = heldDice;
     console.log('holdArr');
     console.log(holdArr);
     diceArr = [];
     diceArr.push.apply(diceArr, holdArr);
+    holdArr = [];
     console.log('diceArr');
     console.log(diceArr);
-    holdArr = [];
+    for (let curDice = 0; curDice < diceHeld; curDice++) {
+      console.log('curDice: ' + curDice + ' diceHeld: ' + diceHeld);
+      let curDiceElement = document.getElementsByClassName('dice')[curDice];
+      curDiceElement.src = diceArr[curDice].imgSrc;
+      console.log('Slot ' + curDice + ' changed to ' + curDice + "'s img");
+      // document.getElementById('hold' + curDice).classList.remove('text-opacity');
+      disableDiceHold(diceArr[curDice]);
+    }
   } else if (rollRemaining == true) {
     farkleEndButton.removeAttribute('disabled');
   }
-  handRankingHeading.style.display = 'none';
+  if (rollScoreHeading.innerText != '0') {
+    saveCurrentRollScore(rollScoreHeading.innerText);
+    console.log('saved score');
+  }
+  rollRankingHeading.style.display = 'none';
   rollScoreHeading.style.display = 'none';
-  for (var currentDice = 0; currentDice < 6; currentDice++) {
+  for (var currentDice = 0 + diceHeld; currentDice < 6; currentDice++) {
     let currentDiceElement = document.getElementsByClassName('dice')[currentDice];
     let holdCurrentDice = document.getElementById('hold' + currentDice);
-    if (!diceArr[currentDice]) {
-      currentDiceElement.classList.remove('spin-grow');
-      holdCurrentDice.classList.add('text-opacity');
-      diceArr.push(new Dice(roll(), currentDiceElement));
-      setTimeout(function() {animateDice(currentDiceElement);}, 20);
-    } else {
-      holdCurrentDice.classList.remove('text-opacity');
-    }
+    currentDiceElement.classList.remove('spin-grow');
+    holdCurrentDice.classList.add('text-opacity');
+    diceArr.push(new Dice(roll(), currentDiceElement));
+    setTimeout(function() {animateDice(currentDiceElement);}, 20);
     currentDiceElement.src = diceArr[currentDice].imgSrc;
   }
-  handRankingHeading.innerText = getRollValues();
+  rollRankingHeading.innerText = getRollValues();
   rollScoreHeading.innerText = getRollScore();
+  currentRollScore = currentRollScore + parseInt(rollScoreHeading.innerText);
   setTimeout(function() {
-    if (handRankingHeading.innerText == 'Farkle') {
-      handRankingHeading.style.color = 'crimson';
+    if (rollRankingHeading.innerText == 'Farkle') {
+      rollRankingHeading.style.color = 'crimson';
     } else {
-      handRankingHeading.style.color = 'darkblue';
+      rollRankingHeading.style.color = 'darkblue';
     }
-    handRankingHeading.style.display = 'block';
+    rollRankingHeading.style.display = 'block';
 
     if (rollScoreHeading.innerText != '0') {
       rollScoreHeading.style.display = 'block';
@@ -68,6 +82,12 @@ function farkleRoll() {
     }
   }, 350);
   diceRolls++;
+  if (rollRankingHeading.innerText == 'Farkle') {
+    rollRemaining = false;
+    diceRollButton.innerText = 'Play Again';
+    farkleEndButton.setAttribute('disabled', 'disabled');
+    diceRollButton.onclick = function() {window.location.reload();}
+  }
   setTimeout(function() {diceRollButton.disabled = false;}, 300);
 }
 
@@ -94,11 +114,11 @@ function animateDice(diceElement) {
 
 function getRollValues() {
   let resultText = 'Farkle';
-  const highestSameKindCount = classifySameKinds(diceArr);
+  const highestSameKindCount = classifySameKinds();
   let isStraightRoll;
-  let isAllPairsRoll = isAllPairs(diceArr);
-  let isTripletsRoll = isTwoTriplets(diceArr);
-  highestSameKindCount == 1 ? isStraightRoll = true : isStraightRoll = false;
+  let isAllPairsRoll = isAllPairs();
+  let isTripletsRoll = isTwoTriplets();
+  highestSameKindCount == 1 && diceHeld == 0 ? isStraightRoll = true : isStraightRoll = false;
   let isRollScoring = classifyDiceWorth(highestSameKindCount, isAllPairsRoll, isTripletsRoll, isStraightRoll);
   if (isStraightRoll) {
     resultText = 'Straight';
@@ -118,12 +138,12 @@ function getRollValues() {
     resultText = 'Scoring Dice Rolled';
   }
   return resultText;
-  function classifySameKinds(diceArr) {
-    for (var i = 0; i < 6; i++) {
+  function classifySameKinds() {
+    for (var i = 0 + diceHeld; i < 6; i++) {
       var currentKindCount = 1;
-      for (let j = 5; j >= 0; j--) {
-        if (i !== j) {
-          if (diceArr[i].numValue === diceArr[j].numValue) {
+      for (let j = 5; j >= 0 + diceHeld; j--) {
+        if (i != j) {
+          if (diceArr[i].numValue == diceArr[j].numValue) {
             currentKindCount++;
           }
         }
@@ -138,10 +158,16 @@ function getRollValues() {
         diceArr[i].hasTriple = false;
       }
     }
-    let maxMatches = Math.max(diceArr[0].sameKindCount, diceArr[1].sameKindCount, diceArr[2].sameKindCount, 
-      diceArr[3].sameKindCount, diceArr[4].sameKindCount, diceArr[5].sameKindCount);
-    for (let i = 0; i < 6; i++) {
-      if (maxMatches == 1) {
+    var maxArr = [];
+    for (let x = 0 + diceHeld; x < 6; x++) {
+      let curValue = diceArr[x].sameKindCount;
+      maxArr.push(curValue);
+    }
+    // let maxMatches = Math.max(diceArr[0].sameKindCount, diceArr[1].sameKindCount, diceArr[2].sameKindCount, 
+    //   diceArr[3].sameKindCount, diceArr[4].sameKindCount, diceArr[5].sameKindCount);
+      let maxMatches = Math.max(...maxArr);
+    for (let i = 0 + diceHeld; i < 6; i++) {
+      if (maxMatches == 1 && diceHeld == 0) {
         diceArr[i].isStraight = true;
         enableDiceHold(diceArr[i]);
       } else {
@@ -150,16 +176,22 @@ function getRollValues() {
     }
     return maxMatches;
   }
-  function isAllPairs(diceArr) {
-    for (let i = 0; i < 6; i++) {
+  function isAllPairs() {
+    if (diceHeld != 0) {
+      return false;
+    }
+    for (let i = 0 + diceHeld; i < 6; i++) {
       if (!diceArr[i].hasPair) {
         return false;
       }
     }
     return true;
   }
-  function isTwoTriplets(diceArr) {
-    for (let i = 0; i < 6; i++) {
+  function isTwoTriplets() {
+    if (diceHeld != 0) {
+      return false;
+    }
+    for (let i = 0 + diceHeld; i < 6; i++) {
       if (!diceArr[i].hasTriple) {
         return false;
       }
@@ -168,7 +200,7 @@ function getRollValues() {
   }
   function classifyDiceWorth(highestSameKind, isAllPairs, isTriplets, isStraight) {
     let scoringDiceExists = false;
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0 + diceHeld; i < 6; i++) {
       if (highestSameKind < 3) {
         if (diceArr[i].numValue == 1 || diceArr[i].numValue == 5) {
           enableDiceHold(diceArr[i]);
@@ -195,7 +227,6 @@ function getRollValues() {
         } else if (diceArr[i].sameKindCount == 3) {
           enableDiceHold(diceArr[i]);
           if (diceArr[i].numValue == 1) {
-            diceArr[i].multipleDiceWorth = 6 * currentBet;
             diceArr[i].singleDiceWorth = 2 * currentBet;
           } else {
             diceArr[i].multipleDiceWorth = (diceArr[i].numValue * 2) * currentBet;
@@ -233,21 +264,70 @@ function getRollValues() {
 function getRollScore() {
   let score = 0;
   let multipleDiceScored = false;
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0 + diceHeld; i < 6; i++) {
     if (diceArr[i].allDiceWorth != 0) {
       return diceArr[i].allDiceWorth;
     } else if (diceArr[i].multipleDiceWorth != 0 && multipleDiceScored == false) {
       score = score + diceArr[i].multipleDiceWorth;
       multipleDiceScored = true;
     } else if (diceArr[i].singleDiceWorth != 0) {
-      score = score + diceArr[i].singleDiceWorth;
+      if (diceArr[i].numValue == 5 && diceArr[i].sameKindCount > 2) {
+        console.log('already counted fives as a group');
+      } else {
+        score = score + diceArr[i].singleDiceWorth;
+      }
     }
   }
   return score;
 }
 
+function saveCurrentRollScore(score) {
+  if (diceRolls > 0) {
+    const span1 = document.createElement('span'), span2 = document.createElement('span');
+    span1.className = 'sm-text farkle-turn-tally'; span2.className = 'result sm-text farkle-turn-tally';
+    rollScoreDiv.appendChild(span1);
+    rollScoreDiv.appendChild(span2);
+    span1.innerText = document.getElementsByClassName('result').length + ': ';
+    span2.innerHTML = score + '<br>';
+    rollScoreDiv.style.display = 'block';
+  }
+}
+
+function endTurn() {
+  endTurnConfirm(currentRollScore);
+}
+
+function endTurnConfirm(score) {
+  const dialog = document.getElementById('confirmDialog');
+  if (typeof dialog.showModal === 'function') {
+    dialog.showModal();
+    setTimeout(function() {dialog.close()}, 30000);
+  } else {
+    console.log('The <dialog> API is not supported by this browser');
+  }
+  document.getElementById('confirmButton').onclick = function() {
+    dialog.close();
+    endTurnDialog(score);
+  }
+}
+
+function endTurnDialog(score) {
+  const dialog = document.getElementById('endTurnDialog');
+  document.getElementById('endTurnPopupSpan').innerText = score;
+  if (typeof dialog.showModal === 'function') {
+    dialog.showModal();
+    setTimeout(function() {dialog.close(); window.location.reload();}, 5000);
+  } else {
+    console.log('The <dialog> API is not supported by this browser');
+  }
+  document.getElementById('endTurnCancel').onclick = function() {
+    dialog.close();
+    window.location.reload();
+  }
+}
+
 function disableDiceHold(...args) {
-  for (let i=0; i < args.length; i++) {
+  for (let i = 0; i < args.length; i++) {
     args[i].canHold = false;
     args[i].diceElement.classList.remove('interactive-img');
     // args[i].diceElement.removeAttribute('onclick');
@@ -256,7 +336,7 @@ function disableDiceHold(...args) {
 }
 
 function enableDiceHold(...args) {
-  for (let i=0; i < args.length; i++) {
+  for (let i = 0; i < args.length; i++) {
     args[i].canHold = true;
     let holdId = args[i].diceElement.parentElement.childNodes[1].getAttribute('id');
     args[i].diceElement.classList.add('interactive-img');
